c29a1c93bba645451d06c99fb978c82b4e4905ec
Robert.Molloy <Robert.Molloy@emeal.nttdata.com>
2025-04-17 18:13:31 +0100

    layout improved

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,12 +15,8 @@ export function Layout(p: { children: React.ReactNode; showLeftSidebar: boolean 
             <Sidebar />
           </aside>
         )}
-        <main className="h-[calc(100vh-3.5rem)]">
-          <div className={"flex h-full flex-col"}>
-            <div className="flex-1 overflow-y-auto p-3">
-              <div className="p-6">{p.children}</div>
-            </div>
-          </div>
+        <main className="h-[calc(100vh-3.5rem)] overflow-y-auto">
+          <div className="p-6">{p.children}</div>
         </main>
       </div>
       <RightSidebar isOpen={isOpen} onClose={close} />
